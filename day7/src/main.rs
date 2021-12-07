b28b6d08cc9f63ab937b4e7b5dadedda782c2e1b
Matt Corbin <mcorbin93@gmail.com>
2021-12-07 00:12:06 -0500

    day 7 solutions

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,3 +1,46 @@
+use std::cmp::min;
+use std::fs;
+
+fn part1(input: &str) {
+    let positions: Vec<u64> = input.split(",").map(|x| x.parse().unwrap()).collect();
+    let highest_pos = *positions.iter().max().unwrap();
+    let lowest_post = *positions.iter().min().unwrap();
+    let mut min_fuel = u64::MAX;
+    for i in lowest_post..highest_pos {
+        let mut fuel_required = 0;
+        for pos in &positions {
+            if *pos > i {
+                fuel_required += *pos-i;
+            } else {
+                fuel_required += i-*pos;
+            }
+        }
+        min_fuel = min(min_fuel, fuel_required)
+    }
+    println!("part1: {}", min_fuel);
+}
+
+fn part2(input: &str) {
+    let positions: Vec<u64> = input.split(",").map(|x| x.parse().unwrap()).collect();
+    let highest_pos = *positions.iter().max().unwrap();
+    let lowest_post = *positions.iter().min().unwrap();
+    let mut min_fuel = u64::MAX;
+    for i in lowest_post..highest_pos {
+        let mut fuel_required = 0;
+        for pos in &positions {
+            if *pos > i {
+                fuel_required += (0..=*pos-i).sum::<u64>();
+            } else {
+                fuel_required += (0..=i-*pos).sum::<u64>();
+            }
+        }
+        min_fuel = min(min_fuel, fuel_required)
+    }
+    println!("part1: {}", min_fuel);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input = fs::read_to_string("input.txt").expect("Unable to read input");
+    part1(&input);
+    part2(&input);
 }
